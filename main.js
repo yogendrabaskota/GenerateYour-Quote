392f89c6012e1e8bd654a840b6a22a5cdd455ef3
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-05-07 14:31:32 +0545

    quote added for different categories

---
+++ main.js
@@ -0,0 +1,23 @@
+const quotes = {
+    science: [
+        "Science is a way of thinking much more than it is a body of knowledge. - Carl Sagan",
+        "The important thing is to never stop questioning. – Albert Einstein",
+        "Somewhere, something incredible is waiting to be known. – Carl Sagan"
+    ],
+    philosophy: [
+        "The unexamined life is not worth living. – Socrates",
+        "Happiness depends upon ourselves. – Aristotle",
+        "He who thinks great thoughts, often makes great errors. – Martin Heidegger"
+    ],
+    motivation: [
+        "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",
+        "The future depends on what you do today. – Mahatma Gandhi",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill"
+    ],
+    life: [
+        "Life is what happens when you're busy making other plans. – John Lennon",
+        "In the end, we only regret the chances we didn’t take.",
+        "To live is the rarest thing in the world. Most people exist, that is all. – Oscar Wilde"
+    ]
+};
+
